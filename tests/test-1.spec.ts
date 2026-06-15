d6c2bd809c74806d5c6bfa85b3cac45a1dbf489b
madhu048 <geramadhu048@gmail.com>
2026-06-15 12:47:46 +0530

    Increase timeout for 'View profile' link click in Playwright test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -17,7 +17,7 @@ test('test', async ({ page }) => {
     await expect(page.locator('.btn-primary.loginButton')).toHaveText('Login');
     await page.getByRole('button', { name: 'Login', exact: true }).click();
     await page.waitForTimeout(5000);
-    await page.getByRole('link', { name: 'View profile' }).click();
+    await page.getByRole('link', { name: 'View profile' }).click({timeout: 10000});
     await expect(page.locator('#lazyAttachCV')).toContainText('Update resume');
     const [fileChooser] = await Promise.all([
       page.waitForEvent('filechooser'),
